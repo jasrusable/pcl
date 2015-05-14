29626e535195f3356241f86e1965043669582c62
Jason Russell <jasrusable@gmail.com>
2015-05-14 13:41:20 +0200

    new stuffs

---
+++ tmp/build/CMakeFiles/pcd_write.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jason/git/pcl/tmp/pcd_write.cpp" "/home/jason/git/pcl/tmp/build/CMakeFiles/pcd_write.dir/pcd_write.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_QHULL"
+  "DISABLE_VTK"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/jason/tmp/pcl-pcl-1.7.2/build/include"
+  "/usr/include/eigen3"
+  "/home/jason/tmp/pcl-pcl-1.7.2/common/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/kdtree/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/octree/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/search/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/sample_consensus/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/filters/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/features/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/keypoints/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/io/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/geometry/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/segmentation/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/surface/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/registration/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/recognition/include"
+  "/home/jason/tmp/pcl-pcl-1.7.2/tracking/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
